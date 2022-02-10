2cd8c691a66fc3e03370c5881728d0b674f0dca0
Rukartik <kartiknavgire.r12@gmail.com>
2022-02-10 14:56:19 +0530

    LoginPage updated

--- V-Tiger/src/main/java/pomRepo/LoginPage.java
+++ V-Tiger/src/main/java/pomRepo/LoginPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
 /***
  * 
  * @author kartik
@@ -41,7 +40,6 @@ public class LoginPage {
 	}
 
 	// action methods or business logics or page action
-	@Test
 	public void LoginToApp(String username, String password) {
 		usernameTextField.sendKeys(username);
 		passwordTextField.sendKeys(password);
